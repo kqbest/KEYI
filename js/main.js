ca9c8d046ce56bb4903ef644bf5ba9c1ba78497e
kqbest <2381149830@qq.com>
2018-01-21 14:18:29 +0800

    'c'

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(function(){
+	//下载点击事件
 	$('#dowloa_btn').click(function(){
 		$('.popupBox').show();
 	});
-
+	//弹框关闭点击事件
 	$('.close').click(function(){
 		$('.popupBox').hide();
-		console.log(1111)
 	});
+	//判断屏幕宽度，当宽度小于1024时改变title的显示方式
+	var innerWidth = window.innerWidth;
+	if (innerWidth >= 1024){
+		$('.title_pc').show();
+		$('.title_wap').hide();
+	}else{
+		$('.title_pc').hide();
+		$('.title_wap').show();
+	}
 });
