9081fd80e695b0d13862de415cf0817c07e4fd12
kqbest <2381149830@qq.com>
2018-01-21 22:08:11 +0800

    '1'

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 $(function(){
 	//下载点击事件
-	$('#dowloa_btn').click(function(){
-		$('.popupBox').show();
+	$('#download_btn').click(function(){
+		$('.popup_box').show();
 	});
 	//弹框关闭点击事件
 	$('.close').click(function(){
-		$('.popupBox').hide();
+		$('.popup_box').hide();
 	});
 	//判断屏幕宽度，当宽度小于1024时改变title的显示方式
-	var innerWidth = window.innerWidth;
-	if (innerWidth >= 1024){
+	if (window.innerWidth >= 1024){
 		$('.title_pc').show();
-		$('.title_wap').hide();
 	}else{
-		$('.title_pc').hide();
 		$('.title_wap').show();
-		$('.main').css('backgroundImage','url(./img/bg_wap.png)');
+		$('.section1').css('backgroundImage','url(./img/bg_wap.png)');
 	}
+	//针对首屏的logo居中
+	$('.section1_cnt').css('marginTop', -$('.section1_cnt').innerHeight()/2);
+	//适配简易版的全屏
+	$('.whole_screen').css('height',window.innerHeight - $('.footer').innerHeight());
 });
