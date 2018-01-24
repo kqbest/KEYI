d3334880470370f91f360d17c50f8628ae62dece
kqbest <2381149830@qq.com>
2018-01-24 17:22:39 +0800

    '1'

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ $(function(){
 	}else{
 		$('.title_wap').show();
 		$('.section1').css('backgroundImage','url(./img/bg_wap.png)');
+		$('.anchor1').css({backgroundImage:'url(./img/bg_wap1.png)',with:'100%'});
+		$('.anchor4').css({backgroundImage:'url(./img/bg_wap2.png)',with:'100%'});
 	}
 	//针对首屏的logo居中
 	$('.section1_cnt').css('marginTop', -$('.section1_cnt').innerHeight()/2);
