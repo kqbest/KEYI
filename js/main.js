23fd099877f0fa1fcaf876ec46687313eb151e16
kqbest <2381149830@qq.com>
2018-01-30 21:44:36 +0800

    '1'

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ if (window.innerWidth >= 1024){
 		$('.section1_download').addClass('animation2');
 	});
 }
-
 //判断屏幕宽度，当宽度小于1024时改变title的显示方式
 if (window.innerWidth >= 1024){
 	$('.title_pc').show();
@@ -29,19 +28,23 @@ if (window.innerWidth >= 1024){
 	$('.anchor1').css({backgroundImage:'url(./img/bg_wap1.jpg)',with:'100%'});
 	$('.anchor4').css({backgroundImage:'url(./img/bg_wap2.jpg)',with:'100%'});
 }
-
 //针对首屏的logo居中
 $('.section1_cnt').css('marginTop', -$('.section1_cnt').innerHeight()/2);
-
 //适配简易版的全屏
 if (window.innerHeight > 568){
 	$('.whole_screen').css('height',window.innerHeight - $('.footer').innerHeight());
 }
-
 //根据鼠标滚动距离执行动画
 var innerHeight = window.innerHeight;
-$('.main').scroll(function(){
-	var top = $(this).scrollTop();
+window.onscroll = function(){
+	animate();
+};
+window.ontouchmove = function(){
+	animate();
+};
+//动画函数
+function animate(){
+	var top = $(window).scrollTop();
 	if(top >= innerHeight/3){
 		$('.anchor1').addClass('animation1');
 		$('.anchor2').addClass('animation2');
@@ -55,13 +58,11 @@ $('.main').scroll(function(){
 	if(top >= innerHeight*2+innerHeight/3){
 		$('.anchor7').addClass('animation1');
 	}
-});
-
+}
 //下载点击事件
 $('#download_btn').click(function(){
 	$('.popup_box').show();
 });
-
 //弹框关闭点击事件
 $('.close').click(function(){
 	$('.popup_box').hide();
